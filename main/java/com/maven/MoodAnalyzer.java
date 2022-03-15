b9981a36bc5057c07e80b624e2577ad1925a4dea
Ranidhumma <ranidhumma8@gmail.com>
2022-03-15 23:21:51 +0530

    Happy and SAD mood

--- main/java/com/maven/MoodAnalyzer.java
+++ main/java/com/maven/MoodAnalyzer.java
@@ -11,4 +11,17 @@ public class MoodAnalyzer {
 			return null;
 		}
 	}
+
+	/*
+	 * in this main method we created MoodAnalyser Object and called analyseMood
+	 * method with passing parameters to print Happy and SAD mood
+	 */
+
+	public static void main(String[] args) {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		String mood = moodAnalyzer.analyseMood("I am Happy");
+		System.out.println(mood);
+		mood = moodAnalyzer.analyseMood("I am Sad");
+		System.out.println(mood);
+	}
 }
